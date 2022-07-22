5cc074c00b29e18bb39511015882f395a3e783e1
angelique KRAUSE <krause_angelique@hotmail.com>
2022-07-22 14:04:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // All credits accorded to https://github.com/wesbos & big thanks !
+let key = document.querySelector('kbd');
+let sound = document.getElementsByClassName('sound');
+
+kbd.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    log.textContent += `${e.code};`
+}
+// voir dans les credits la solution car le coach ne la donne pas 
